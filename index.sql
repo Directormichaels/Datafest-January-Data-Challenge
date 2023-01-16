925a262a40426c20e21ac0e07ac2e78ddc6e07f1
Directormichaels <105192767+Directormichaels@users.noreply.github.com>
2023-01-16 01:35:14 +0100

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,29 @@
+-- 1. How many orders were shipped by Speedy Express in total?
+SELECT COUNT(*)
+FROM Orders
+JOIN Shippers
+ON Orders.ShipperID = Shippers.ShipperID
+WHERE ShipperName = "Speedy Express";
+
+
+-- 2. What was the last name of the employee with the most orders?
+SELECT Last name,COUNT(*) AS Number_of_Orders
+FROM Orders
+JOIN Employees
+ON Orders.EmployeeID = Employees.EmployeeID
+GROUP BY LastName
+ORDER BY Number_of_Orders DESC
+LIMIT 1;
+
+
+-- 3. What product was ordered the most by customers in Germany?
+SELECT Products.ProductName, Orderdetails.Quantity
+FROM Customers JOIN Orders
+ON Customers.CustomerID = Orders.CustomerID
+JOIN Orderdetails
+ON Orders.OrderID = Orderdetails.OrderID
+JOIN Products
+ON Orderdetails.ProductID = Products.ProductID
+WHERE Country = "Germany"
+ORDER BY Quantity DESC
+LIMIT 1;
